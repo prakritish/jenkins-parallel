a3696ba67ec89f409bb43e478ed96bd30ae7d098
Prakritish Sen Eshore <prakritish@seneshore.com>
2023-10-08 20:50:53 +0530

    removing  Post

--- vars/nodeBuild.groovy
+++ vars/nodeBuild.groovy
@@ -1,14 +1,13 @@
 def call(String nodeVersion, String label) {
     def parallelStage = {
-        stage("Node: ${nodeVersion}, OS: ${label}") {
-            agent {
-                label "${label}"
-            }
-            stage("Build") {
-                echo "Building on OS: ${label} with Node Version: ${nodeVersion}"
-            }
-            stage("Report") {
-                echo "Build completed!"
+        node("${label}") {
+            stage("Node: ${nodeVersion}, OS: ${label}") {
+                stage("Build") {
+                    echo "Building on OS: ${label} with Node Version: ${nodeVersion}"
+                }
+                stage("Report") {
+                    echo "Build completed!"
+                }
             }
         }
     }
