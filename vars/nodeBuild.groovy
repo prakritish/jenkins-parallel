757737334e75d93a57bfd5eb1628dc478e855414
Prakritish Sen Eshore <prakritish@seneshore.com>
2023-10-08 18:26:40 +0530

    removing  Post

--- vars/nodeBuild.groovy
+++ vars/nodeBuild.groovy
@@ -7,6 +7,9 @@ def call(String nodeVersion, String label) {
             stage("Build") {
                 echo "Building on OS: ${label} with Node Version: ${nodeVersion}"
             }
+            stage("Report") {
+                echo "Build completed!"
+            }
         }
     }
     return parallelStage
